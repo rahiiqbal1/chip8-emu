9f447c97d3bbc58ef21d9f1966c356a52227e7d3
Rahi Iqbal <rahi.iqbal1@gmail.com>
2024-06-08 16:06:46 +0100

    Create bitmap in main

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,22 @@
 const std = @import("std");
 const Display = @import("display.zig").Display;
+const Bitmap = @import("bitmap.zig").Bitmap;
 
 pub fn main() !void {
+    // General purpose memory allocator:
+    const gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer {
+        const leaked = gpa.deinit();
+        if (leaked == .leak) {
+            @panic("Memory leak\n");
+        }
+    }
+
+    // Creating bitmap:
+    var bitmap = try Bitmap.create(allocator, 64, 32);
+    defer bitmap.free();
+
     // Initialising display:
     var display = try Display.create("CHIP-8", 800, 400);
     defer display.free();
