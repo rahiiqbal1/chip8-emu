6f30f5b96fdb21002b4282464647ef1408ec28a4
Rahi Iqbal <rahi.iqbal1@gmail.com>
2024-06-08 12:03:49 +0100

    Write skeleton and doc for Dxyn (not implemented)

--- src/cpu.zig
+++ src/cpu.zig
@@ -226,6 +226,17 @@ const CPU = struct {
                 vx.* = rand_num & kk;
                 self.registers.incrementPC();
             },
+            // (Dxyn) DRW Vx, Vy, nibble. Display n-byte sprite starting at
+            // memory location I at (Vx, Vy), set VF = collision. Read n bytes
+            // from memory, starting at the address stored in I. These are then
+            // displayed on screen at (Vx, Vy). Sprites are XORed onto the 
+            // screen. If this causes any pixels to be erased, VF is set to 1,
+            // otherwise to 0. If the sprite is positioned so part of it is 
+            // outside the coordinates of the display, it wraps around to the
+            // opposite side of the screen: todo
+            0xD => {
+
+            },
         }
     }
 };
