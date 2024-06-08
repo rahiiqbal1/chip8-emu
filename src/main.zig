7abd9866cd3eeb4b766421524f06e9d67e36356e
Rahi Iqbal <rahi.iqbal1@gmail.com>
2024-06-08 15:45:58 +0100

    Add basic SDL window creation in main

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,13 @@
 const std = @import("std");
+const Display = @import("display.zig").Display;
 
 pub fn main() !void {
-    
+    // Initialising display:
+    var display = try Display.create("CHIP-8", 800, 400);
+    defer display.free();
+
+    // Display loop:
+    while (display.open == true) {
+        display.input();
+    }
 }
