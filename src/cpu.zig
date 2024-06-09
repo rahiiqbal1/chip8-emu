d35b1909a7e169f6e4a1a4271607d2678887af78
Rahi Iqbal <rahi.iqbal1@gmail.com>
2024-06-09 10:59:16 +0100

    Make cpu public

--- src/cpu.zig
+++ src/cpu.zig
@@ -40,7 +40,7 @@ const Registers = struct {
     }
 };
 
-const CPU = struct {
+pub const CPU = struct {
     // Memory. 4096 bytes available. The first 512 bytes (0x000 to 0x1FF) are
     // where the original interpreter was located and should not be used by
     // programs:
