a7fac5d5f499547bd198a6bad99ef5db3d76198a
Rahi Iqbal <rahi.iqbal1@gmail.com>
2024-06-10 17:22:30 +0100

    Change to using flag rom; some errors

--- src/main.zig
+++ src/main.zig
@@ -26,7 +26,7 @@ pub fn main() !void {
 
     // Creating device and loading rom:
     var chip8: Device = Device.create(&bitmap, &display);
-    _ = try chip8.loadROM("/home/rahi/projs/chip8-emu/roms/pong.rom");
+    _ = try chip8.loadROM("/home/rahi/projs/chip8-emu/roms/4-flags.ch8");
 
     // Display loop:
     const fps: f32 = 60.0;
